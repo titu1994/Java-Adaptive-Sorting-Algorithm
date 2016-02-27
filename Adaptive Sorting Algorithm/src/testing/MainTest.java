7bb41d57275d945b8986c853a6e9b9bf13796ffd
Somshubra Majumdar <titu1994@gmail.com>
2016-02-27 09:56:02 +0530

    Cleaned up MainTest

--- Adaptive Sorting Algorithm/src/testing/MainTest.java
+++ Adaptive Sorting Algorithm/src/testing/MainTest.java
@@ -7,79 +7,109 @@ public class MainTest {
 	public static void main(String[] args) {
 
 		ExecutionManager.processArrayOfType(50, 0, "50-Random");
-		System.out.println("Random done");
+		System.out.println("50 Random done");
 		ExecutionManager.processArrayOfType(50, 1, "50-AlmostSorted-1");
-		System.out.println("Almost Sorted 1 done");
+		System.out.println("50 Almost Sorted 1 done");
 		ExecutionManager.processArrayOfType(50, 2, "50-AlmostSorted-2");
-		System.out.println("Almost Sorted 2 done");
+		System.out.println("50 Almost Sorted 2 done");
 		ExecutionManager.processArrayOfType(50, 3, "50-AlmostSorted-3");
-		System.out.println("Almost Sorted 3 done");
+		System.out.println("50 Almost Sorted 3 done");
 		ExecutionManager.processArrayOfType(50, 4, "50-AlmostSorted-4");
-		System.out.println("Almost Sorted 4 done");
+		System.out.println("50 Almost Sorted 4 done");
 		ExecutionManager.processArrayOfType(50, 5, "50-AlmostSorted-5");
-		System.out.println("Almost Sorted 5 done");
+		System.out.println("50 Almost Sorted 5 done");
 		ExecutionManager.processArrayOfType(50, 6, "50-ReverseSorted");
-		System.out.println("Finished 50");
+		System.out.println("50 Finished");
 
 		ExecutionManager.processArrayOfType(100, 0, "100-Random");
+		System.out.println("100 Random done");
 		ExecutionManager.processArrayOfType(100, 1, "100-AlmostSorted-1");
+		System.out.println("100 Almost Sorted 1 done");
 		ExecutionManager.processArrayOfType(100, 2, "100-AlmostSorted-2");
+		System.out.println("100 Almost Sorted 2 done");
 		ExecutionManager.processArrayOfType(100, 3, "100-AlmostSorted-3");
+		System.out.println("100 Almost Sorted 3 done");
 		ExecutionManager.processArrayOfType(100, 4, "100-AlmostSorted-4");
+		System.out.println("100 Almost Sorted 4 done");
 		ExecutionManager.processArrayOfType(100, 5, "100-AlmostSorted-5");
+		System.out.println("100 Almost Sorted 5 done");
 		ExecutionManager.processArrayOfType(100, 6, "100-ReverseSorted");
-
-		System.out.println("Finished 100");
+		System.out.println("100 Finished");
 
 		ExecutionManager.processArrayOfType(500, 0, "500-Random");
-		System.out.println("Random done");
+		System.out.println("500 Random done");
 		ExecutionManager.processArrayOfType(500, 1, "500-AlmostSorted-1");
-		System.out.println("Almost Sorted 1 done");
+		System.out.println("500 Almost Sorted 1 done");
 		ExecutionManager.processArrayOfType(500, 2, "500-AlmostSorted-2");
-		System.out.println("Almost Sorted 2 done");
+		System.out.println("500 Almost Sorted 2 done");
 		ExecutionManager.processArrayOfType(500, 3, "500-AlmostSorted-3");
-		System.out.println("Almost Sorted 3 done");
+		System.out.println("500 Almost Sorted 3 done");
 		ExecutionManager.processArrayOfType(500, 4, "500-AlmostSorted-4");
-		System.out.println("Almost Sorted 4 done");
+		System.out.println("500 Almost Sorted 4 done");
 		ExecutionManager.processArrayOfType(500, 5, "500-AlmostSorted-5");
-		System.out.println("Almost Sorted 5 done");
+		System.out.println("500 Almost Sorted 5 done");
 		ExecutionManager.processArrayOfType(500, 6, "500-ReverseSorted");
-		System.out.println("Finished 500");
+		System.out.println("500 Finished");
 
 		ExecutionManager.processArrayOfType(1000, 0, "1000-Random");
+		System.out.println("1000 Random done");
 		ExecutionManager.processArrayOfType(1000, 1, "1000-AlmostSorted-1");
+		System.out.println("1000 Almost Sorted 1 done");
 		ExecutionManager.processArrayOfType(1000, 2, "1000-AlmostSorted-2");
+		System.out.println("1000 Almost Sorted 2 done");
 		ExecutionManager.processArrayOfType(1000, 3, "1000-AlmostSorted-3");
+		System.out.println("1000 Almost Sorted 3 done");
 		ExecutionManager.processArrayOfType(1000, 4, "1000-AlmostSorted-4");
+		System.out.println("1000 Almost Sorted 4 done");
 		ExecutionManager.processArrayOfType(1000, 5, "1000-AlmostSorted-5");
+		System.out.println("1000 Almost Sorted 5 done");
 		ExecutionManager.processArrayOfType(1000, 6, "1000-ReverseSorted");
-
-		System.out.println("Finished 1000");
+		System.out.println("1000 Finished");
 		
 		ExecutionManager.processArrayOfType(10000, 0, "10000-Random");
+		System.out.println("10000 Random done");
 		ExecutionManager.processArrayOfType(10000, 1, "10000-AlmostSorted-1");
+		System.out.println("10000 Almost Sorted 1 done");
 		ExecutionManager.processArrayOfType(10000, 2, "10000-AlmostSorted-2");
+		System.out.println("10000 Almost Sorted 2 done");
 		ExecutionManager.processArrayOfType(10000, 3, "10000-AlmostSorted-3");
+		System.out.println("10000 Almost Sorted 3 done");
 		ExecutionManager.processArrayOfType(10000, 4, "10000-AlmostSorted-4");
+		System.out.println("10000 Almost Sorted 4 done");
 		ExecutionManager.processArrayOfType(10000, 5, "10000-AlmostSorted-5");
+		System.out.println("10000 Almost Sorted 5 done");
 		ExecutionManager.processArrayOfType(10000, 6, "10000-ReverseSorted");
-
-		System.out.println("Finished 10000");
+		System.out.println("10000 Finished");
 
 		ExecutionManager.processArrayOfType(100000, 0, "100000-Random");
-		System.out.println("Random done");
+		System.out.println("100000 Random done");
 		ExecutionManager.processArrayOfType(100000, 1, "100000-AlmostSorted-1");
-		System.out.println("Almost Sorted 1 done");
+		System.out.println("100000 Almost Sorted 1 done");
 		ExecutionManager.processArrayOfType(100000, 2, "100000-AlmostSorted-2");
-		System.out.println("Almost Sorted 2 done");
+		System.out.println("100000 Almost Sorted 2 done");
 		ExecutionManager.processArrayOfType(100000, 3, "100000-AlmostSorted-3");
-		System.out.println("Almost Sorted 3 done");
+		System.out.println("100000 Almost Sorted 3 done");
 		ExecutionManager.processArrayOfType(100000, 4, "100000-AlmostSorted-4");
-		System.out.println("Almost Sorted 4 done");
+		System.out.println("100000 Almost Sorted 4 done");
 		ExecutionManager.processArrayOfType(100000, 5, "100000-AlmostSorted-5");
-		System.out.println("Almost Sorted 5 done");
+		System.out.println("100000 Almost Sorted 5 done");
 		ExecutionManager.processArrayOfType(100000, 6, "100000-ReverseSorted");
-		System.out.println("Finished 100000");
+		System.out.println("100000 Finished 100000");
+
+		ExecutionManager.processArrayOfType(500000, 0, "500000-Random");
+		System.out.println("500000 Random done");
+		ExecutionManager.processArrayOfType(500000, 1, "500000-AlmostSorted-1");
+		System.out.println("500000 Almost Sorted 1 done");
+		ExecutionManager.processArrayOfType(500000, 2, "500000-AlmostSorted-2");
+		System.out.println("500000 Almost Sorted 2 done");
+		ExecutionManager.processArrayOfType(500000, 3, "500000-AlmostSorted-3");
+		System.out.println("500000 Almost Sorted 3 done");
+		ExecutionManager.processArrayOfType(500000, 4, "500000-AlmostSorted-4");
+		System.out.println("500000 Almost Sorted 4 done");
+		ExecutionManager.processArrayOfType(500000, 5, "500000-AlmostSorted-5");
+		System.out.println("500000 Almost Sorted 5 done");
+		ExecutionManager.processArrayOfType(500000, 6, "500000-ReverseSorted");
+		System.out.println("500000 Finished 100000");
 
 		ExecutionManager.processArrayOfType(1000000, 0, "1000000-Random");
 		System.out.println("1M : Random done");
